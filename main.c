3dfbe5b4fe9d2416562d2f91f1cb58b33adaa5cd
Mihail Mihail <studymiet0@outlook.com>
2025-06-02 17:59:07 +0300

    Lab5.2v2

--- main.c
+++ main.c
@@ -14,10 +14,19 @@ int find_halfnum_l(int M, int N) {
 	return halfnum_l - 1;
 }
 
-int** rand_arr(int** pointer_arr, int M , int N) {
-	int hcount = 0, lcount = 0;
+int** pointer_arr_mem_alloc(int** pointer_arr, int N, int M) {
+	int hcount = 0;
 	while (hcount < N) {
 		pointer_arr[hcount] = (int*)malloc(M * sizeof(int));
+		hcount++;
+	}
+	return pointer_arr;
+}
+
+int** rand_arr(int** pointer_arr, int M , int N) {
+	pointer_arr = pointer_arr_mem_alloc(pointer_arr, N, M);
+	int hcount = 0, lcount = 0;
+	while (hcount < N) {
 		while (lcount < M) {
 			pointer_arr[hcount][lcount] = rand();
 			lcount++;
@@ -28,17 +37,17 @@ int** rand_arr(int** pointer_arr, int M , int N) {
 	return pointer_arr;
 }
 
-int find_less(int** pointer_arr, int *less_arr, int M, int N, int A) {
+int** find_less(int** pointer_arr, int** less_arr, int* less_arr_length, int M, int N, int A) {
 	int hcount = 0, lcount = 0, lesscount=0;
 	pointer_arr = rand_arr(pointer_arr, M, N);
 	while (hcount < N) {
 		while (lcount < M) {
 			if (hcount < find_halfnum_h(M, N) && lcount < find_halfnum_l(M, N) && pointer_arr[hcount][lcount] < A) { 
-				less_arr[lesscount] = pointer_arr[hcount][lcount];
+				less_arr[lesscount] = &pointer_arr[hcount][lcount];
 				lesscount++;
 			}
 			if (hcount > find_halfnum_h(M, N) && lcount > find_halfnum_l(M, N) && pointer_arr[hcount][lcount] < A) {
-				less_arr[lesscount] = pointer_arr[hcount][lcount]; 
+				less_arr[lesscount] = &pointer_arr[hcount][lcount];
 				lesscount++;
 			}
 			lcount++;
@@ -46,16 +55,27 @@ int find_less(int** pointer_arr, int *less_arr, int M, int N, int A) {
 		lcount = 0;
 		hcount++;
 	}
-	return lesscount;
+	less_arr_length = &lesscount;
+	return less_arr;
 }
 
-void print(int** pointer_arr, int *less_arr, int M, int N, int A) {
-	int hcount = 0, lcount = 0, less, linecount=0, lesscount=0, halfnum_h=0, halfnum_l=0;
-	less = find_less(pointer_arr, less_arr, M, N, A);
+int less_on_pointer_arr_match(int** less_arr, int element, int *less_arr_length, int N, int M) {
+	int lesscount = 0;
+	while (lesscount <= *less_arr_length) {
+		if (less_arr[lesscount] == &element) { return 1; }
+		else { return 0; }
+	}
+}
+
+void print(int** pointer_arr, int **less_arr, int less_arr_length, int M, int N, int A) {
+	int hcount = 0, lcount = 0, linecount = 0, lesscount = 0, halfnum_h = 0, halfnum_l = 0, nflag = 0, matchframeflag=0;
+	less_arr = find_less(pointer_arr, less_arr, less_arr_length, M, N, A);
 	halfnum_h = find_halfnum_h(M, N);
 	halfnum_l = find_halfnum_l(M, N);
 	while (hcount < N) {
-		printf("\n");
+		if (nflag == 1) { printf(""); }
+		else { printf("\n"); }
+		nflag = 0;
 		if (M % 2 == 0) { printf("     "); }
 		else { printf(""); }
 		while (linecount < halfnum_l) { printf("          "); linecount++; }
@@ -63,55 +83,122 @@ void print(int** pointer_arr, int *less_arr, int M, int N, int A) {
 		printf("\n"); 
 		linecount = 0;
 		while (lcount < M) {
+			if (less_on_pointer_arr_match(less_arr, pointer_arr[hcount][lcount], less_arr_length, N, M)) { matchframeflag = 1; }
+			else { matchframeflag = 0; }
 			if (N % 2 == 0) {
 				if (M % 2 == 0) {
-					if (lcount == halfnum_l && hcount == halfnum_h) { 
-						while (linecount + 1 < halfnum_l) { 
-							printf("-"); linecount++; 
+					if (lcount == halfnum_l && hcount == halfnum_h) {
+						while (linecount < halfnum_l ) {
+							 linecount++;
 						}
 						linecount = 0;
-						printf("%5d--|--", pointer_arr[hcount][lcount]); 
-						while (linecount + 1 < halfnum_l * 2) {
-							printf("-"); linecount++;
-						}
-						printf("\n");
-						linecount = 0;
+						if (matchframeflag) { printf("|%5d| |  ", pointer_arr[hcount][lcount]); }
+						else { printf("%5d  |  ", pointer_arr[hcount][lcount]); }
 					}
-					else if (lcount == halfnum_l && hcount != halfnum_h) { printf("%5d  |  ", pointer_arr[hcount][lcount]); }
-					else if (lcount != halfnum_l && hcount == halfnum_h) { printf("%5d-----", pointer_arr[hcount][lcount]); }
-					else { printf("%5d     ", pointer_arr[hcount][lcount]); }
+					else if (lcount == halfnum_l && hcount != halfnum_h) { 
+						if (matchframeflag) { printf("|%5d| |  ", pointer_arr[hcount][lcount]); } 
+						else { printf("%5d  |  ", pointer_arr[hcount][lcount]); }
+					}
+					else if (lcount != halfnum_l && hcount == halfnum_h) { 
+						if (lcount == M - 1) {
+							if (matchframeflag) { printf("|%5d|    ", pointer_arr[hcount][lcount]); }
+							else { printf("%5d     ", pointer_arr[hcount][lcount]); }
+							printf("\n");
+							printf("-----");
+							while (linecount < halfnum_l) {
+								printf("----------");
+								linecount++;
+							}
+							printf("--|--");
+							while (linecount < halfnum_l * 2 + 1) {
+								printf("----------");
+								linecount++;
+							}
+							linecount = 0;
+						}
+						else { 
+							if (matchframeflag) { printf("|%5d|    ", pointer_arr[hcount][lcount]); }
+							else { printf("%5d     ", pointer_arr[hcount][lcount]); }
+						}
+					}
+					else { 
+						if (matchframeflag) { printf("|%5d|    ", pointer_arr[hcount][lcount]); }
+						else { printf("%5d     ", pointer_arr[hcount][lcount]); }
+					}
 				}
 				else {
-					if (lcount != halfnum_l && hcount == halfnum_h || lcount == halfnum_l && hcount == halfnum_h) { 
+					if (lcount == halfnum_l && hcount == halfnum_h) {
 						while (linecount < halfnum_l) {
-							printf("-"); linecount++;
+							linecount++;
 						}
 						linecount = 0;
-						printf("%5d-----", pointer_arr[hcount][lcount]); 
-						while (linecount < halfnum_l * 2 + 1) {
-							printf("-"); linecount++;
-						}
-						printf("\n");
-						linecount = 0;
-
+						printf("%5d     ", pointer_arr[hcount][lcount]);
 					}
-					else if (lcount == halfnum_l && hcount != halfnum_h) { printf("%5d  |  ", pointer_arr[hcount][lcount]); }
-					else { printf("%5d     ", pointer_arr[hcount][lcount]); }
+					else if (lcount == halfnum_l && hcount != halfnum_h) {
+						if (matchframeflag) { printf("|%5d|    ", pointer_arr[hcount][lcount]); }
+						else { printf("%5d     ", pointer_arr[hcount][lcount]); }
+					}
+					else if (lcount != halfnum_l && hcount == halfnum_h) {
+						if (lcount == M - 1) {
+							if (matchframeflag) { printf("|%5d|    ", pointer_arr[hcount][lcount]); }
+							else { printf("%5d     ", pointer_arr[hcount][lcount]); }
+							printf("\n");
+							while (linecount < halfnum_l) {
+								printf("----------");
+								linecount++;
+							}
+							printf("--|--");
+							while (linecount < halfnum_l * 2 ) {
+								printf("----------");
+								linecount++;
+							}
+							printf("-----");
+							linecount = 0;
+							nflag = 1;
+						}
+						else { 
+							if (matchframeflag) { printf("|%5d|    ", pointer_arr[hcount][lcount]); }
+							else { printf("%5d     ", pointer_arr[hcount][lcount]); }
+						}
+					}
+					else { 
+						if (matchframeflag) { printf("|%5d|    ", pointer_arr[hcount][lcount]); }
+						else { printf("%5d     ", pointer_arr[hcount][lcount]); }
+					}
 				}
 			}
 			else {
 				if (M % 2 == 0) {
 					if (lcount == halfnum_l && hcount == halfnum_h) {
-						printf("%5d--|--", pointer_arr[hcount][lcount]);
+						if (matchframeflag) { printf("|%5d|-|--", pointer_arr[hcount][lcount]); }
+						else { printf("%5d--|--", pointer_arr[hcount][lcount]); }
 					}
-					else if (lcount == halfnum_l && hcount != halfnum_h) { printf("%5d  |  ", pointer_arr[hcount][lcount]); }
-					else if (lcount != halfnum_l && hcount == halfnum_h) { printf("%5d-----", pointer_arr[hcount][lcount]); }
-					else { printf("%5d     ", pointer_arr[hcount][lcount]); }
+					else if (lcount == halfnum_l && hcount != halfnum_h) { 
+						if (matchframeflag) { printf("|%5d| |  ", pointer_arr[hcount][lcount]); }
+						else { printf("%5d  |  ", pointer_arr[hcount][lcount]); }
+					}
+					else if (lcount != halfnum_l && hcount == halfnum_h) { 
+						if (matchframeflag) { printf("|%5d|----", pointer_arr[hcount][lcount]); }
+						else { printf("%5d-----", pointer_arr[hcount][lcount]); }
+					}
+					else { 
+						if (matchframeflag) { printf("|%5d|    ", pointer_arr[hcount][lcount]); }
+						else { printf("%5d     ", pointer_arr[hcount][lcount]); }
+					}
 				}
 				else {
-					if (lcount != halfnum_l && hcount == halfnum_h || lcount == halfnum_l && hcount == halfnum_h) { printf("%5d-----", pointer_arr[hcount][lcount]); }
-					else if (lcount == halfnum_l && hcount != halfnum_h) { printf("%5d    ", pointer_arr[hcount][lcount]); }
-					else { printf("%5d     ", pointer_arr[hcount][lcount]); }
+					if (lcount != halfnum_l && hcount == halfnum_h || lcount == halfnum_l && hcount == halfnum_h) { 
+						if (matchframeflag) { printf("|%5d|----", pointer_arr[hcount][lcount]); }
+						else { printf("%5d-----", pointer_arr[hcount][lcount]); }
+					}
+					else if (lcount == halfnum_l && hcount != halfnum_h) { 
+						if (matchframeflag) { printf("|%5d|    ", pointer_arr[hcount][lcount]); }
+						else { printf("%5d     ", pointer_arr[hcount][lcount]); }
+					}
+					else { 
+						if (matchframeflag) { printf("|%5d|    ", pointer_arr[hcount][lcount]); }
+						else { printf("%5d     ", pointer_arr[hcount][lcount]); }
+					}
 				}
 			}
 			lcount++;
@@ -124,30 +211,34 @@ void print(int** pointer_arr, int *less_arr, int M, int N, int A) {
 	while (linecount < 20) { printf("="); linecount++; }
 	printf("\n");
 	printf("List of elements less than %d: \n", A);
-	while (lesscount < less) {
-		printf("%5d\n", pointer_arr[lcount][hcount]);
+	while (lesscount < less_arr_length) {
+		printf("%5d\n", **less_arr[lesscount]);
 		lesscount++;
 	}
 }
 
-void clean_pointer_arr(int** pointer_arr, int N) {
+void clean_mem_pointer_subarr(int** pointer_arr, int N) {
 	int hcount = 0;
-	while (hcount < N){free(pointer_arr[hcount]);}
+	while (hcount < N) { free(pointer_arr[hcount]); hcount++; }
 }
 int main(void) {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 	srand(time(NULL));
-	int** pointer_arr, *less_arr, M = 0, N = 0, A=0, line = 0;
-	scanf_s("%d %d %d", &M, &N, &A);
-	while (line < 20) { printf("="); line++; }
-	line = 0;
-	pointer_arr = (int**)malloc(N * sizeof(int*));
-	if (pointer_arr == NULL) { perror("Ошибка выделения памяти"); return 1; }
-	less_arr = (int*)malloc(((N * M)/2 + 1) * sizeof(int*));
-	print(pointer_arr, less_arr, M, N, A);
-	clean_pointer_arr(pointer_arr, N);
-	free(pointer_arr);
-	free(less_arr);
+	int** pointer_arr, **less_arr, *less_arr_length, M = 0, N = 0, A=0, line = 0;
+	while (1) {
+		scanf_s("%d %d %d", &N, &M, &A);
+		while (line < 20) { printf("="); line++; }
+		line = 0;
+		pointer_arr = (int**)malloc(N * sizeof(int*));
+		if (pointer_arr == NULL) { perror("Ошибка выделения памяти"); return 1; }
+		less_arr = (int*)malloc(((N * M) / 2 + 1) * sizeof(int*));
+		print(pointer_arr, *less_arr_length, less_arr, M, N, A);
+		clean_mem_pointer_subarr(pointer_arr, N);
+		free(pointer_arr);
+		free(less_arr);
+		while (line < 20) { printf("V"); line++; }
+		printf("\n");
+	}
 	return 0;
 }
