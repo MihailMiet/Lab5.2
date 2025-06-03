33b8626896b787ac7afee064aa4672447b5930ff
Mihail Mihail <studymiet0@outlook.com>
2025-06-03 07:39:00 +0300

    Lab5.2v3

--- main.c
+++ main.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 #include <windows.h>
 #include <time.h>
-int find_halfnum_h( int M, int N) {
-	int hcount = 0, lcount = 0, halfnum_h; 
+int find_halfnum_h(int N) {
+	int halfnum_h; 
 	if (N % 2 == 0) { halfnum_h = N / 2; } else { halfnum_h = N / 2 + 1; }
-	return halfnum_h - 1 ;
+	return halfnum_h - 1;
 }
 
-int find_halfnum_l(int M, int N) {
-	int hcount = 0, lcount = 0, halfnum_l;
+int find_halfnum_l(int M) {
+	int halfnum_l;
 	if (M % 2 == 0) { halfnum_l = M / 2; } else { halfnum_l = M / 2 + 1; }
 	return halfnum_l - 1;
 }
@@ -23,7 +23,16 @@ int** pointer_arr_mem_alloc(int** pointer_arr, int N, int M) {
 	return pointer_arr;
 }
 
-int** rand_arr(int** pointer_arr, int M , int N) {
+int** less_arr_mem_alloc(int** less_arr, int N, int M) {
+	int hcount = 0;
+	while (hcount < N) {
+		less_arr[hcount] = (int*)malloc(sizeof(int));
+		hcount++;
+	}
+	return less_arr;
+}
+
+int** rand_arr(int** pointer_arr, int N , int M) {
 	pointer_arr = pointer_arr_mem_alloc(pointer_arr, N, M);
 	int hcount = 0, lcount = 0;
 	while (hcount < N) {
@@ -37,41 +46,57 @@ int** rand_arr(int** pointer_arr, int M , int N) {
 	return pointer_arr;
 }
 
-int** find_less(int** pointer_arr, int** less_arr, int* less_arr_length, int M, int N, int A) {
-	int hcount = 0, lcount = 0, lesscount=0;
-	pointer_arr = rand_arr(pointer_arr, M, N);
+int** find_less(int** pointer_arr, int** less_arr, int* less_arr_length, int N, int M, int A) {
+	int hcount = 0, lcount = 0, lesscount=0, halfnum_h, halfnum_l;
+	halfnum_h = find_halfnum_h(N);
+	halfnum_l = find_halfnum_l(M);
+	pointer_arr = rand_arr(pointer_arr, N, M);
+	less_arr = less_arr_mem_alloc(less_arr, N, M);
 	while (hcount < N) {
 		while (lcount < M) {
-			if (hcount < find_halfnum_h(M, N) && lcount < find_halfnum_l(M, N) && pointer_arr[hcount][lcount] < A) { 
-				less_arr[lesscount] = &pointer_arr[hcount][lcount];
-				lesscount++;
+			if (N % 2 == 0 && M % 2 == 0) {
+				if (((hcount <= halfnum_h) && (lcount <= halfnum_l) || (hcount > halfnum_h) && (lcount > halfnum_l)) && pointer_arr[hcount][lcount] < A) {
+					less_arr[lesscount] = &pointer_arr[hcount][lcount];
+					lesscount++;
+				}
 			}
-			if (hcount > find_halfnum_h(M, N) && lcount > find_halfnum_l(M, N) && pointer_arr[hcount][lcount] < A) {
-				less_arr[lesscount] = &pointer_arr[hcount][lcount];
-				lesscount++;
+			else if (N % 2 != 0 && M % 2 == 0) {
+				if (((hcount <= halfnum_h) && (lcount <= halfnum_l) || (hcount >= halfnum_h) && (lcount >= halfnum_l)) && pointer_arr[hcount][lcount] < A) {
+					less_arr[lesscount] = &pointer_arr[hcount][lcount];
+					lesscount++;
+				}
+			}
+			else if (N % 2 == 0 && M % 2 != 0) {
+				if (((hcount <= halfnum_h) && (lcount <= halfnum_l) || (hcount > halfnum_h) && (lcount >= halfnum_l)) && pointer_arr[hcount][lcount] < A) {
+					less_arr[lesscount] = &pointer_arr[hcount][lcount];
+					lesscount++;
+				}
+			}
+			else if (N % 2 != 0 && M % 2 != 0) {
+				if (((hcount <= halfnum_h) && (lcount <= halfnum_l) || (hcount >= halfnum_h) && (lcount >= halfnum_l)) && pointer_arr[hcount][lcount] < A) {
+					less_arr[lesscount] = &pointer_arr[hcount][lcount];
+					lesscount++;
+				}
 			}
 			lcount++;
 		}
 		lcount = 0;
 		hcount++;
 	}
-	less_arr_length = &lesscount;
+	*less_arr_length = lesscount;
 	return less_arr;
 }
 
-int less_on_pointer_arr_match(int** less_arr, int element, int *less_arr_length, int N, int M) {
-	int lesscount = 0;
-	while (lesscount <= *less_arr_length) {
-		if (less_arr[lesscount] == &element) { return 1; }
-		else { return 0; }
-	}
+int less_on_pointer_arr_match(int** less_arr, int** element, int hcount, int lcount, int M) {
+	if (&less_arr[hcount * M + lcount] == &element[hcount][lcount]) { return 1; }
+	else { return 0; }
 }
 
-void print(int** pointer_arr, int **less_arr, int less_arr_length, int M, int N, int A) {
+void print(int** pointer_arr, int **less_arr, int* less_arr_length, int N, int M, int A) {
 	int hcount = 0, lcount = 0, linecount = 0, lesscount = 0, halfnum_h = 0, halfnum_l = 0, nflag = 0, matchframeflag=0;
-	less_arr = find_less(pointer_arr, less_arr, less_arr_length, M, N, A);
-	halfnum_h = find_halfnum_h(M, N);
-	halfnum_l = find_halfnum_l(M, N);
+	less_arr = find_less(pointer_arr, less_arr, less_arr_length, N, M, A);
+	halfnum_h = find_halfnum_h(N);
+	halfnum_l = find_halfnum_l(M);
 	while (hcount < N) {
 		if (nflag == 1) { printf(""); }
 		else { printf("\n"); }
@@ -83,7 +108,7 @@ void print(int** pointer_arr, int **less_arr, int less_arr_length, int M, int N,
 		printf("\n"); 
 		linecount = 0;
 		while (lcount < M) {
-			if (less_on_pointer_arr_match(less_arr, pointer_arr[hcount][lcount], less_arr_length, N, M)) { matchframeflag = 1; }
+			if (less_on_pointer_arr_match(less_arr, pointer_arr, hcount, lcount, M)) { matchframeflag = 1; }
 			else { matchframeflag = 0; }
 			if (N % 2 == 0) {
 				if (M % 2 == 0) {
@@ -201,6 +226,7 @@ void print(int** pointer_arr, int **less_arr, int less_arr_length, int M, int N,
 					}
 				}
 			}
+			matchframeflag = 0;
 			lcount++;
 		}
 		lcount = 0;
@@ -211,8 +237,8 @@ void print(int** pointer_arr, int **less_arr, int less_arr_length, int M, int N,
 	while (linecount < 20) { printf("="); linecount++; }
 	printf("\n");
 	printf("List of elements less than %d: \n", A);
-	while (lesscount < less_arr_length) {
-		printf("%5d\n", **less_arr[lesscount]);
+	while (lesscount < *less_arr_length) {
+		printf("%5d\n", less_arr[lesscount][0]);
 		lesscount++;
 	}
 }
@@ -221,22 +247,31 @@ void clean_mem_pointer_subarr(int** pointer_arr, int N) {
 	int hcount = 0;
 	while (hcount < N) { free(pointer_arr[hcount]); hcount++; }
 }
+
+void clean_mem_less_subarr(int* less_arr, int* less_arr_length) {
+	int hcount = 0;
+	while (hcount < *less_arr_length) { free(less_arr[hcount]); hcount++; }
+	
+}
+
 int main(void) {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 	srand(time(NULL));
-	int** pointer_arr, **less_arr, *less_arr_length, M = 0, N = 0, A=0, line = 0;
+	int** pointer_arr = NULL, **less_arr = NULL, *less_arr_length = NULL, M = 0, N = 0, A=0, line = 0;
 	while (1) {
 		scanf_s("%d %d %d", &N, &M, &A);
 		while (line < 20) { printf("="); line++; }
 		line = 0;
 		pointer_arr = (int**)malloc(N * sizeof(int*));
 		if (pointer_arr == NULL) { perror("Ошибка выделения памяти"); return 1; }
-		less_arr = (int*)malloc(((N * M) / 2 + 1) * sizeof(int*));
-		print(pointer_arr, *less_arr_length, less_arr, M, N, A);
+		less_arr = (int**)malloc(N * M * sizeof(int*));
+		less_arr_length = (int*)malloc(sizeof(int*));
+		print(pointer_arr, less_arr, less_arr_length, N, M, A);
+		free(less_arr);
+		free(less_arr_length);
 		clean_mem_pointer_subarr(pointer_arr, N);
 		free(pointer_arr);
-		free(less_arr);
 		while (line < 20) { printf("V"); line++; }
 		printf("\n");
 	}
